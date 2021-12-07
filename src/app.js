2ae789e5c26f70615fa7b7a31366d18e8aff0865
sumit-0-0-1 <sumit.email.dev@gmail.com>
2021-12-07 21:54:46 +0530

    app tested and made changes in ui

--- src/app.js
+++ src/app.js
@@ -3,29 +3,41 @@ const showBtn = document.querySelector("#btn");
 const show = document.querySelector("#show-output");
 
 // function 1
+// that converts input date into DD/MM/YY format and convert it into string
+function dayMonthYear() {
+    const date = new Date(inputDate.value);
+    const day = date.getDate().toString();
+    const month = (date.getMonth() + 1).toString();
+    const year = date.getFullYear().toString().slice(-2);
+    const ddmmyy = day + month + year;
+    return ddmmyy;
+}
+
+// function 2
 //that takes a string and reverses it
-function reverseStr(dateIntoStr) {
-    const reversedStr = dateIntoStr.split('').reverse().join('');
-    return reversedStr;
+function reverseStr() {
+    const y = dayMonthYear();
+    const reversedStr = y.split('').reverse().join('');
+    return show.innerText = reversedStr;
 }
 
-
-
-// function 3
-// that converts input date into DD/MM/YY format
-function dateMonthYear() {
-    const date = inputDate.value;
-    const x1 = new Date(date);
-    const day = x1.getDate().toString();
-    const month = (x1.getMonth() + 1).toString();
-    const year = x1.getFullYear().toString();
-    const newddmmyy = day + month + year;
-    return newddmmyy;
-}
-
+//function 3
+// that checks palindrome
 function output() {
-    const x = dateMonthYear();
-    return console.log(x);
+    const inputDate = dayMonthYear();
+    const reversedDate = reverseStr();
+    if (inputDate > 0) {
+
+        if (inputDate === reversedDate) {
+            return show.innerText = "Wow, Your birthday is palindrome!";
+
+        } else if (inputDate != reversedDate) {
+            return show.innerText = "Ops! Your birthday is not palindrome!";
+        }
+    } else {
+        show.innerText = "Please select the date";
+    }
+
 }
 
 showBtn.addEventListener('click', output);
